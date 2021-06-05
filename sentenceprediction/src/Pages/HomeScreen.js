2bdea99a824c5fe18c5eca1dc5ebb198250c1813
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-06-05 11:34:22 +0530

    fixed word count

--- sentenceprediction/src/Pages/HomeScreen.js
+++ sentenceprediction/src/Pages/HomeScreen.js
@@ -6,6 +6,12 @@ export default function HomeScreen() {
     const [response,setResponse]=useState('');
     const [written,setWritten]=useState(0);
     const [predicted,setPredicted]=useState(0);
+    const countWords=(str)=>{
+        str = str.replace(/(^\s*)|(\s*$)/gi,"");
+        str = str.replace(/[ ]{2,}/gi," ");
+        str = str.replace(/\n /,"\n");
+        return str.split(' ').length;
+    }
     const handleChange= async (e)=>{
         setUserText(e.target.value);
             if (/\s$/.test(e.target.value)) {
@@ -17,18 +23,18 @@ export default function HomeScreen() {
               setPredicted(predicted+data.words);
               console.log(data);
             }
-            setWritten(written+e.target.value.split(' ').length)
+            setWritten(countWords(e.target.value))
         console.log(e.target.value);
     }
   return (
     <article className="landing">
       <section className="title">
         <div className="titleText">Predly</div>
-        <div className="Prediction Details">
-          <div className="subtitleText">{written} Words Written</div>
-          <div className="subtitleText">{predicted} Words predicted</div>
-        </div>
       </section>
+        <div style={{display:"flex",flexDirection:"column",justifyContent:"start", alignItems:"start", padding:"20px 0"}} className="Prediction Details">
+          <div className="subtitleText">{written} Words Written</div>
+          <div className="subtitleText">{predicted} Words Predicted</div>
+        </div>
       <section className="questionSection">
         <textarea
           className="inputTextBox"
@@ -44,7 +50,7 @@ export default function HomeScreen() {
         </div>
       </section>
       <section class="footer">
-        <div classname="answer-text">
+        <div className="answer-text">
           Made with ❤ by Rishabh, Hemanth and Saksham
         </div>
       </section>
