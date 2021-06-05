5bd58452e6640dc64acab490ce823c0b6bbfa2ef
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-06-05 11:06:51 +0530

    added backend integration

--- sentenceprediction/src/Pages/HomeScreen.js
+++ sentenceprediction/src/Pages/HomeScreen.js
@@ -1,7 +1,19 @@
 
 import './HomeScreen.css'
+import {useState} from 'react';
 export default function HomeScreen() {
-    const handleChange=(e)=>{
+    const [userText,setUserText]=useState('');
+    const [response,setResponse]=useState('');
+    const handleChange= async (e)=>{
+        await setUserText(e.target.value);
+            if (/\s$/.test(e.target.value)) {
+              const response = await fetch(
+                `http://20.98.240.49:5000/predict/?text=${e.target.value}`
+              );
+              const data = await response.json();
+              setResponse(data.prediction);
+              console.log(data);
+            }
         console.log(e.target.value);
     }
   return (
@@ -10,15 +22,21 @@ export default function HomeScreen() {
         <div className="titleText">Predly</div>
       </section>
       <section className="questionSection">
-        <textarea className="inputTextBox" type="textarea" 
+        <textarea
+          className="inputTextBox"
+          type="textarea"
           name="textValue"
+          value={userText}
           onChange={handleChange}
         />
       </section>
       <section class="answerSection">
         <div className="answerContainer">
-            <span className="answer-text"></span>
+          <span className="answer-text">{response}</span>
         </div>
+      </section>
+      <section class="footer">
+        <div classname="answer-text">Made with ❤ by Rishabh, Hemanth and Saksham</div>
       </section>
     </article>
   );
