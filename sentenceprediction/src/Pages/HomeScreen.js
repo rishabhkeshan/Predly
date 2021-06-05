dd1be64f4bc3bfb8f300d2d646cea59f21d1655a
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-06-05 11:20:05 +0530

    added words predicted and written count

--- sentenceprediction/src/Pages/HomeScreen.js
+++ sentenceprediction/src/Pages/HomeScreen.js
@@ -4,22 +4,30 @@ import {useState} from 'react';
 export default function HomeScreen() {
     const [userText,setUserText]=useState('');
     const [response,setResponse]=useState('');
+    const [written,setWritten]=useState(0);
+    const [predicted,setPredicted]=useState(0);
     const handleChange= async (e)=>{
-        await setUserText(e.target.value);
+        setUserText(e.target.value);
             if (/\s$/.test(e.target.value)) {
               const response = await fetch(
                 `http://20.98.240.49:5000/predict/?text=${e.target.value}`
               );
               const data = await response.json();
               setResponse(data.prediction);
+              setPredicted(predicted+data.words);
               console.log(data);
             }
+            setWritten(written+e.target.value.split(' ').length)
         console.log(e.target.value);
     }
   return (
     <article className="landing">
       <section className="title">
         <div className="titleText">Predly</div>
+        <div className="Prediction Details">
+          <div className="subtitleText">{written} Words Written</div>
+          <div className="subtitleText">{predicted} Words predicted</div>
+        </div>
       </section>
       <section className="questionSection">
         <textarea
@@ -36,7 +44,9 @@ export default function HomeScreen() {
         </div>
       </section>
       <section class="footer">
-        <div classname="answer-text">Made with ❤ by Rishabh, Hemanth and Saksham</div>
+        <div classname="answer-text">
+          Made with ❤ by Rishabh, Hemanth and Saksham
+        </div>
       </section>
     </article>
   );
